187905999e080c05020f5013fb78be8b5044805c
jessicalealR <jessikrojaz99@gmail.com>
2024-12-10 08:21:03 -0500

    .

--- Sogamove-Backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
-    SQLALCHEMY_DATABASE_URI = 'mysql://user:password@localhost/sogamove'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
